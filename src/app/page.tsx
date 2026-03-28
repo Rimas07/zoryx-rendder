bd32b9f23c37673b4ab6579d4ec9198f1eab46cc
Rimas07 <53482924+Rimas07@users.noreply.github.com>
2026-03-28 16:25:06 +0100

    Sort by rank

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import { getClinics } from '../lib/firebase';
+import { getClinics, getSpecializations } from '../lib/firebase';
 import { ClinicsLayout } from '../components/ClinicsLayout';
 
 
 export const dynamic = "force-static";
 
 export default async function HomePage() {
-  const clinics = await getClinics().catch(() => []);
-  return <ClinicsLayout initialClinics={clinics} />;
+  const [clinics, orderedSpecs] = await Promise.all([
+    getClinics().catch(() => []),
+    getSpecializations().catch(() => []),
+  ]);
+  return <ClinicsLayout initialClinics={clinics} orderedSpecs={orderedSpecs} />;
 }
